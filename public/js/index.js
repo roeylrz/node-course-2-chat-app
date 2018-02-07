118bd67ffedfb88416ce5aea881da1ad16d9f929
roeylrz <35033332+roeylrz@users.noreply.github.com>
2018-02-07 23:17:47 +0200

    aad new message and message events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+//io() - we can use it after loading the library socket.io (script src="/socket.io/socket.io.js"). 
+//when we call it we initiate the request from the client to the server to open up a web socket and keep that connection open.
+var socket = io();
+
+socket.on('connect', function() {
+    console.log('Connected to server')
+
+    socket.emit('createMessage', {
+        from: 'orli',
+        text: 'Hey, how are you ?'
+    });
+});
+socket.on('disconnect',function () {
+    console.log('Disconnected from server')
+});
+socket.on('newMessage', function(createMessage){
+    console.log('newMessage', createMessage);
+})
--- server/server.js
+++ server/server.js
@@ -7,12 +7,22 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();//To enable configuration of Express application
 var server = http.createServer(app);
-var io = socketIO(server);//This way we can do everything we want in terms of emmiting or listening to events
+var io = socketIO(server);//This way we can do everything we want in terms of emiting or listening to events
 
 app.use(express.static(publicPath));//To configure the Express Static Midleware
 
 io.on('connection', (socket) => {
     console.log('New user connected');
+
+    socket.emit('newMessage', {//emit - socket has this method in order to emit events (and not listening to events like on)
+        from: 'roey',
+        text: 'Hey. How are you ?',
+        createAt: 123123
+    }); 
+
+    socket.on('createMessage', (message) => {
+        console.log('create message', message);
+    })
 
     socket.on('disconnect', () => {
         console.log('User was disconnected');
